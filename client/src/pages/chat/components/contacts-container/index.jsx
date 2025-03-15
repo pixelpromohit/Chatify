ac4fe9f036a7aa7f1673f6fc21bb373d2803bf6f
Mohit Ramchandani <ramchandanimohit771@gmail.com>
2025-03-15 16:22:51 +0530

    direct messages modal

--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ProfileInfo from './components/profile-info';
+import NewDM from './components/new-dm';
 
 const ContactsContainer = () => {
     return <div className='relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full'>
@@ -9,6 +10,7 @@ const ContactsContainer = () => {
         <div className="my-5">
             <div className="flex items-center justify-between pr-10">
                 <Title text="Direct Messages"/>
+                <NewDM />
             </div>
         </div>
 
