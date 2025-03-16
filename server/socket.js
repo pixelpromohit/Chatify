f8a9292a2cc06f30c20b2402f7f5e9ccf5848fa3
Mohit Ramchandani <ramchandanimohit771@gmail.com>
2025-03-16 19:07:38 +0530

    socket setup frontend

--- server/socket.js
+++ server/socket.js
@@ -1,41 +1,39 @@
-import { disconnect } from "mongoose"
-import { Server as SocketIOServer } from "socket.io"
-
+import { Server as SocketIOServer } from "socket.io";
 
 const setupSocket = (server) => {
-    const io = new SocketIOServer(server, 
-        {cors : {
-            origin : process.env.ORIGIN,
-            methods : ["GET", "POST"],
-            credentials : true,
-
-        }
-    })
+    const io = new SocketIOServer(server, {
+      cors: {
+        origin: process.env.ORIGIN,
+        methods: ["GET", "POST"],
+        credentials: true,
+      },
+    });
 
     const userSocketMap = new Map()
 
     const disconnect = (socket) => {
-        console.log(`Client Disconneted: ${socket.id}`)
+        console.log("Client disconnected", socket.id);
         for (const [userId, socketId] of userSocketMap.entries()) {
-            if (socketId === socket.id) {
-                userSocketMap.delete(userId)
-                break
-            }
+          if (socketId === socket.id) {
+            userSocketMap.delete(userId);
+            break;
+          }
         }
-    }
+    };
 
     io.on("connection", (socket) => {
-        const userId = socket.handshake.query.userId
-
+        const userId = socket.handshake.query.userId;
+    
         if (userId) {
-            userSocketMap.set(userId, socket.id)
-            console.log(`User Connected : ${userId} with SOCKET ID: ${socket.id}`)
+          userSocketMap.set(userId, socket.id);
+          console.log(userId)
+          console.log(`User connected: ${userId} with socket ID: ${socket.id}`);
         } else {
-            console.log("User ID not provided during connection.")
+          console.log("User ID not provided during connection.");
         }
 
-        socket.on("disconnect", () => disconnect(socket))
-    })
+        socket.on("disconnect", () => disconnect(socket));
+    });
 }
 
 export default setupSocket
